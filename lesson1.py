1b2d68dec5935071145844f34eae4ab6f5e40161
jibeken <kenjibek1606@icloud.com>
2024-12-18 12:42:52 +0600

    first commit

---
+++ lesson1.py
@@ -0,0 +1,6 @@
+name = input('Enter your name:')
+age = input('How old are you?')
+country = input('Where are you from?')
+study = input("Where do you study?")
+film = input("Your favorite film?")
+print('Hello', name, "!", "You're", age, "years.", "You're from", country, '.', "Your favorite film is:", film, "!")
